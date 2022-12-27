3895998b4c6bbd3be819265bf25f4953cae56a63
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-12-27 02:21:41 +0100

    Functionality V1.0

--- Script/main.js
+++ Script/main.js
@@ -37,3 +37,28 @@ window.addEventListener("scroll", () => {
   let navbar =document.getElementById("navbar");
   navbar.classList.toggle("fixed", this.window.scrollY > 0);
 });
+
+
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+  
+});
+
+ScrollReveal().reveal('.text-holder-bg', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('.text-holder-sm', {delay: 700, origin: 'right'});
+ScrollReveal().reveal('.social-icons', {delay: 800, origin: 'left', interval: 200});
+ScrollReveal().reveal('.send', {delay: 800, origin: 'right'});
+ScrollReveal().reveal('.about-col-1', {delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.proficiency-text, #rock-skills', {delay: 500, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.proficiency-sub, #rock-skills-text', {delay: 600, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.profi-textbg', {delay: 700, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.profi-textsm, .skills_container', {delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.proficiency-text', {delay: 800, origin: 'bottom'});
+ScrollReveal().reveal('.timeline-content', {delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.timeline-location', {delay: 600, origin: 'top', interval: 200});
+ScrollReveal().reveal('.timeline-date', {delay: 500, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('.contact-left', {delay: 600, origin: 'left', interval: 200});
+ScrollReveal().reveal('.contact-right', {delay: 700, origin: 'right', interval: 200});
